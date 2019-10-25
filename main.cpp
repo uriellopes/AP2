e946904cf86e1f4ad1d306da6dc7f8633135e572
Uriel Lopes <uriel.lopes@imd.ufrn.br>
2019-10-25 14:57:54 -0300

    Criar Classe Veiculo e Carro

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 
 int main() {
 
-    Carro c;
+    Carro c = Carro("asdasdsad", 1233.12, 54545, "14/02/2019", 2);
+
+    std::cout << c.getMarca() << std::endl;
+    std::cout << c.getPreco() << std::endl;
+    std::cout << c.getChassi() << std::endl;
+    std::cout << c.getData() << std::endl;
+    std::cout << c.getMotor() << std::endl;
 
     Moto m;
 
--- veiculo.h
+++ veiculo.h
@@ -3,21 +3,65 @@ using std::string;
 
 //Classe Veiculo
 class Veiculo {
-    private:
+    protected:
         string marca;
         float preco;
         int chassi;
         string data;
+    public:
+        string getMarca();
+        float getPreco();
+        int getChassi();
+        string getData(); 
 };
 ////
+
+string Veiculo::getMarca() {
+    return marca;
+}
+
+float Veiculo::getPreco() {
+    return preco;
+}
+
+int Veiculo::getChassi() {
+    return chassi;
+}
+
+string Veiculo::getData() {
+    return data;
+}
 
 //Classe Carro
 class Carro : public Veiculo {
     private:
         int motor;
     public:
+        Carro(string m, float p, int c, string d, int motor);
+        ~Carro();
+        string getMotor();
 };
 ////
+
+Carro::Carro(string m, float p, int c, string d, int mt) {
+    marca = m;
+    preco = p;
+    chassi = c;
+    data = d;
+    motor = mt;
+}
+
+Carro::~Carro() {
+
+}
+
+string Carro::getMotor() {
+    if( motor == 1 ) {
+        return "gasolina";
+    } else {
+        return "eletrico";
+    }
+}
 
 //Classe Moto
 class Moto : public Veiculo {
