f1906ed8b4d5834ad2be0c76e47b9e6f2277330d
Uriel Lopes <uriel.lopes@imd.ufrn.br>
2019-10-25 15:06:50 -0300

    Classes Moto e Caminhao criadas

--- main.cpp
+++ main.cpp
@@ -2,19 +2,33 @@
 #include <iostream>
 #include "concessionaria.h"
 
+using namespace std;
+
 int main() {
 
     Carro c = Carro("asdasdsad", 1233.12, 54545, "14/02/2019", 2);
 
-    std::cout << c.getMarca() << std::endl;
-    std::cout << c.getPreco() << std::endl;
-    std::cout << c.getChassi() << std::endl;
-    std::cout << c.getData() << std::endl;
-    std::cout << c.getMotor() << std::endl;
+    cout << c.getMarca() << endl;
+    cout << c.getPreco() << endl;
+    cout << c.getChassi() << endl;
+    cout << c.getData() << endl;
+    cout << c.getMotor() << endl << endl;
 
-    Moto m;
+    Moto m = Moto("asdasdsad", 1233.12, 54545, "14/02/2019", 1);
 
-    Caminhao cm;
+    cout << m.getMarca() << endl;
+    cout << m.getPreco() << endl;
+    cout << m.getChassi() << endl;
+    cout << m.getData() << endl;
+    cout << m.getModelo() << endl << endl;
+
+    Caminhao cm = Caminhao("asdasdsad", 1233.12, 54545, "14/02/2019", 1);
+
+    cout << cm.getMarca() << endl;
+    cout << cm.getPreco() << endl;
+    cout << cm.getChassi() << endl;
+    cout << cm.getData() << endl;
+    cout << cm.getCarga() << endl << endl;
 
     return 0;
 }
--- veiculo.h
+++ veiculo.h
@@ -43,6 +43,7 @@ class Carro : public Veiculo {
 };
 ////
 
+//Funções da classe Carro
 Carro::Carro(string m, float p, int c, string d, int mt) {
     marca = m;
     preco = p;
@@ -62,13 +63,39 @@ string Carro::getMotor() {
         return "eletrico";
     }
 }
+////
 
 //Classe Moto
 class Moto : public Veiculo {
     private:
         int modelo;
     public:
+        Moto(string m, float p, int c, string d, int md);
+        ~Moto();
+        string getModelo();
 };
+////
+
+//Funções da classe Moto
+Moto::Moto(string m, float p, int c, string d, int md) {
+    marca = m;
+    preco = p;
+    chassi = c;
+    data = d;
+    modelo = md;
+}
+
+Moto::~Moto() {
+
+}
+
+string Moto::getModelo() {
+    if( modelo == 1 ) {
+        return "classico";
+    } else {
+        return "esportivo";
+    }
+}
 ////
 
 //Classe Caminhao
@@ -76,5 +103,30 @@ class Caminhao : public Veiculo {
     private:
         int carga;
     public:
+        Caminhao(string m, float p, int c, string d, int cg);
+        ~Caminhao();
+        string getCarga();
 };
+////
+
+//Funções da classe Caminhao
+Caminhao::Caminhao(string m, float p, int c, string d, int cg) {
+    marca = m;
+    preco = p;
+    chassi = c;
+    data = d;
+    carga = cg;
+}
+
+Caminhao::~Caminhao() {
+
+}
+
+string Caminhao::getCarga() {
+    if( carga == 1 ) {
+        return "comum";
+    } else {
+        return "perigosa";
+    }
+}
 ////
