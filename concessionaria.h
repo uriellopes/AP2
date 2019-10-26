d0f802a831c4ddd48810504f8f875873843fb992
uriellopes <urielrlps@gmail.com>
2019-10-26 13:29:47 -0300

    - Criar funcao para salvar dados em um arquivo - Criar funcao para receber dados de um arquivo

--- concessionaria.h
+++ concessionaria.h
@@ -10,23 +10,37 @@ class Concessionaria {
     public:
         Concessionaria(string n, long long int c);
         ~Concessionaria();
+        string getNome();
+        long long int getCNPJ();
         friend std::ostream& operator<< (std::ostream &o, Concessionaria const c);
 };
 
+//Construtor da classe Concessionaria
 Concessionaria::Concessionaria(string n, long long int c) {
     nome = n;
     cnpj = c;
     qtd_estoque = 0;
 }
 
+//Destrutor da classe Concessionaria
 Concessionaria::~Concessionaria() {
 
+}
+
+//Getter do atributo nome
+string Concessionaria::getNome() {
+    return nome;
+}
+
+//Getter do atributo cnpj
+long long int Concessionaria::getCNPJ() {
+    return cnpj;
 }
 
 //Sobrecarga do operador << na classe Concessionaria
 std::ostream& operator<< (std::ostream &o, Concessionaria const c) {
     o << "Nome: " << c.nome << std::endl;
     o << "CNPJ: " << c.cnpj << std::endl;
-    o << "Quantidade no estoque: " << c.qtd_estoque << std::endl;
+    o << "Quantidade no estoque: " << c.qtd_estoque;
     return o;
 }
--- main.cpp
+++ main.cpp
@@ -6,33 +6,55 @@
 
 using namespace std;
 
+//Funcao para ler dados iniciais em um arquivo
+void lerDados(vector<Concessionaria> &concessionarias) {
+    
+    //Abrir arquivo concessionarias.txt em modo de leitura
+    ifstream dadosConcessionaria("concessionarias.txt");
+
+    //Extrair informacoes do arquivo concessionarias.txt para salvar no vector concessionarias
+    vector<string> values;
+    string line, value;
+    while(!dadosConcessionaria.eof()) {
+        getline(dadosConcessionaria, line);
+        if ( !line.empty() ) {
+            stringstream temp(line);
+            values.clear();
+            while(getline(temp, value, ',')) {
+                values.push_back(value);
+            }
+            concessionarias.push_back(Concessionaria(values[0], stoll(values[1], nullptr, 10)));
+        }
+    }
+
+    //Fechar arquivo concessionarias.txt após terminar de extrair as informacoes
+    dadosConcessionaria.close();
+}
+
+void salvarDados(vector<Concessionaria> &concessionarias) {
+
+    //Abrir arquivo concessionarias.txt em modo de escrita
+    ofstream dadosConcessionaria("concessionarias.txt");
+
+    //Salvar dados do vetor concessionaria dentro do arquivo concessionarias.txt
+    for (auto i = concessionarias.begin(); i != concessionarias.end(); i++) {
+        dadosConcessionaria << i->getNome() << "," << i->getCNPJ() << endl;        
+    }
+
+    //Fechar arquivo após salvar os dados
+    dadosConcessionaria.close();
+}
+
 int main() {
 
     //Vector para armazenar todas as concessionarias e suas informacoes
     vector<Concessionaria> concessionarias;
 
-    //Abrir arquivo concessionarias.txt
-    ifstream dados("concessionarias.txt");
+    //Chamar funcao para pegar dados inicias em um arquivo
+    lerDados(concessionarias);
 
-    //Extrair informacoes do arquivo concessionarias.txt para salvar no vector concessionarias
-    vector<string> values;
-    string line, value;
-    while(!dados.eof()) {
-        getline(dados, line);
-        stringstream temp(line);
-        values.clear();
-        while(getline(temp, value, ',')) {
-            values.push_back(value);
-        }
-        concessionarias.push_back(Concessionaria(values[0], stoll(values[1], nullptr, 10)));
-    }
+    //Chamar funcao para salvar os dados em um arquivo
+    salvarDados(concessionarias);
 
-    //Fechar arquivo concessionarias.txt após terminar de extrair as informacoes
-    dados.close();
-
-    cout << concessionarias[0] << endl;
-    cout << concessionarias[1] << endl;
-    cout << concessionarias[2] << endl;
-    
     return 0;
 }
