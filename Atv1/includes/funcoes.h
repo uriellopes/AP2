b12470258a43473b61ba7f1b60f5ee8fdd7a6208
uriellopes <urielrlps@gmail.com>
2019-10-28 13:26:32 -0300

    Finalizando projeto

--- Atv1/includes/funcoes.h
+++
@@ -1,5 +0,0 @@
-#include "concessionaria.h"
-
-void addInfo(std::vector<Concessionaria> &concessionarias);
-void showMenu(std::vector<Concessionaria> &concessionarias);
-void adicionarNovoCarro(Concessionaria &c);
